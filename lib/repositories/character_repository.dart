efc4617e796c015b5d0221c6f5cf051f7e8b4985
Jomazao <jomazao@gmail.com>
2020-10-27 19:52:32 -0500

    added limit to getAll

---
+++ lib/repositories/character_repository.dart
@@ -0,0 +1,145 @@
+import 'package:widgets_basicos/models/character.dart';
+
+class CharacterRepository {
+  final List<Character> _CHARACTERS = [
+    Character(
+        name: 'Goku Black',
+        photo:
+            'https://vignette.wikia.nocookie.net/dragonball/images/6/6e/Goku_Black_DBS.png/revision/latest/scale-to-width-down/232?cb=20180117015759&path-prefix=es',
+        age: 35,
+        occupation: 'Kaio-shin',
+        origin: 'Kaishin',
+        universe: 10,
+        race: 'Shin-jhin',
+        description:
+            'Zamas (ザマスKan, ZamasuHepJP), apodado y mejor conocido como Goku Black (ゴクウブラックKan, Gokū Burakku[12]HepJP, Goku Oscuro[13]), referido regularmente simplemente como Black (ブラックKan, BurakkuHepJP, Oscuro), es, bajo su identidad auténtica, el equivalente de Zamas de la línea de tiempo del presente inalterado, un antiguo Kaio del Norte y aprendiz de Kaio-shin del Universo 10, quien, tras asesinar a su mentor Gowas y convertirse en Kaio-shin, utilizó el anillo del tiempo para viajar al Año 780 y usar las Super Esferas del Dragón con el objetivo de intercambiar de cuerpos con Son Goku, asesinándolo a él junto a su esposa e hijo menor, tomando así su identidad y poder para efectuar el Plan Cero Humanos en colaboración con su versión del futuro alternativo y así conseguir aniquilar a los humanos en el omniverso.'),
+    Character(
+        name: 'Karin',
+        photo:
+            'https://vignette.wikia.nocookie.net/dragonball/images/c/cf/KarinHoldingDragonBall.png/revision/latest?cb=20110901011507&path-prefix=es',
+        age: 800,
+        race: 'Humanoide',
+        occupation: 'Maestro de la torre de Karin',
+        universe: 7,
+        origin: 'Tierra',
+        description:
+            'Karin (カリンKan, KarinHepJP), también llamado Duende Karin, Korin, Matou Malí (en España) o Whiskers el Gato Maravilla inicialmente en Hispanoamérica, es un gato blanco, genio de las artes marciales, mundialmente famoso que vive en la tierra sagrada. Hace su aparición en el anime de Dragon Ball en el Episodio 61. Solo entrenaría a el que lograse subir a la Torre y llegar a él. Su fuerza de combate es de 169, un poder superior a el de Maestro Roshi y Tao Pai Pai. Cuando Goku fue derrotado por Tao Pai Pai subió a la torre en la cual consiguió superar la supuesta prueba de quitarle a Karin la jarra con el agua que "daba más poder", pero realmente lo que le dio mas poder fue los tres días que tardó en conseguirlo, ya que el agua era normal y corriente. El Maestro Karin también entrenó al Maestro Roshi, pero el no tardó 3 días sino 3 años. Karin después de la muerte de Piccolo también entrena a Tenshinhan, Krilin, Yamcha y Chaoz.'),
+    Character(
+        name: 'Janemba',
+        photo:
+            'https://vignette.wikia.nocookie.net/dragonball/images/8/8a/Janemba_artwork_SDBH.png/revision/latest?cb=20200630222641&path-prefix=es',
+        race: 'Demonio',
+        age: 300,
+        origin: 'Otro mundo',
+        universe: 7,
+        occupation: 'Villano',
+        description:
+            'Janemba (ジャネンバKan, JanenbaHepJP) es un demonio de pura maldad y gran poder maligno. Creado a partir de las acciones de un adolescente ogro llamado Psyche Oni, quien era el responsable de vigilar la máquina purificadora de almas, la cual explota como consecuencia de la falta de cuidado del ogro, las almas malignas terminan por usar como huésped su cuerpo, creando así a este demonio. \nEs el antagonista principal del largometraje de Dragon Ball Z: ¡El renacer de la fusión! Goku y Vegeta.'),
+    Character(
+        name: 'Gogeta',
+        race: 'Saiyan',
+        age: 38,
+        origin: 'Planeta tierra',
+        occupation: 'Fusión',
+        universe: 7,
+        description:
+            'En la película Dragon Ball Z: ¡El renacer de la fusión! Goku y Vegeta, la parte de las hombreras y cuello eran de color anaranjado, representado a Goku. Además, usa un pantalón blanco-grisáceos ligeramente oscurecidos que es sujetado por una cinta azul-turquesa agua y porta tobilleras también de color azul, representado el color de Vegeta. Por último, la fusión utiliza muñequeras negras de cuero y zapatos del mismo tono como calzado. En su diseño original, dibujando una línea vertical en su rostro se lo puede dividir en partes simétricas, distinguiendo a Goku en el lado derecho y a Vegeta en el izquierdo. Su cabello es similar al de Vegetto, disponiendo del peinado de ambos de sus individuos componentes en cada costado, pero teniendo un mechón en la frente en vez de dos además la terminación del cabello es en forma de pico de viuda, como el de Vegeta. Su cuerpo es similar al de Goku, es alto y delgado y al momento de transformarse en Supersaiyano mantiene el color de piel de é',
+        photo:
+            'https://i.pinimg.com/originals/e7/b6/ee/e7b6ee64b87b0b302cc0560882d80f8e.gif'),
+    Character(
+        name: 'Jocu blac',
+        photo:
+            'https://vignette.wikia.nocookie.net/dragonball/images/6/6e/Goku_Black_DBS.png/revision/latest/scale-to-width-down/232?cb=20180117015759&path-prefix=es',
+        age: 35,
+        occupation: 'Kaio-shin',
+        origin: 'Kaishin',
+        universe: 10,
+        race: 'Shin-jhin',
+        description:
+            'Zamas (ザマスKan, ZamasuHepJP), apodado y mejor conocido como Goku Black (ゴクウブラックKan, Gokū Burakku[12]HepJP, Goku Oscuro[13]), referido regularmente simplemente como Black (ブラックKan, BurakkuHepJP, Oscuro), es, bajo su identidad auténtica, el equivalente de Zamas de la línea de tiempo del presente inalterado, un antiguo Kaio del Norte y aprendiz de Kaio-shin del Universo 10, quien, tras asesinar a su mentor Gowas y convertirse en Kaio-shin, utilizó el anillo del tiempo para viajar al Año 780 y usar las Super Esferas del Dragón con el objetivo de intercambiar de cuerpos con Son Goku, asesinándolo a él junto a su esposa e hijo menor, tomando así su identidad y poder para efectuar el Plan Cero Humanos en colaboración con su versión del futuro alternativo y así conseguir aniquilar a los humanos en el omniverso.'),
+    Character(
+        name: 'Marin',
+        photo:
+            'https://vignette.wikia.nocookie.net/dragonball/images/c/cf/KarinHoldingDragonBall.png/revision/latest?cb=20110901011507&path-prefix=es',
+        age: 800,
+        race: 'Humanoide',
+        occupation: 'Maestro de la torre de Karin',
+        universe: 7,
+        origin: 'Tierra',
+        description:
+            'Karin (カリンKan, KarinHepJP), también llamado Duende Karin, Korin, Matou Malí (en España) o Whiskers el Gato Maravilla inicialmente en Hispanoamérica, es un gato blanco, genio de las artes marciales, mundialmente famoso que vive en la tierra sagrada. Hace su aparición en el anime de Dragon Ball en el Episodio 61. Solo entrenaría a el que lograse subir a la Torre y llegar a él. Su fuerza de combate es de 169, un poder superior a el de Maestro Roshi y Tao Pai Pai. Cuando Goku fue derrotado por Tao Pai Pai subió a la torre en la cual consiguió superar la supuesta prueba de quitarle a Karin la jarra con el agua que "daba más poder", pero realmente lo que le dio mas poder fue los tres días que tardó en conseguirlo, ya que el agua era normal y corriente. El Maestro Karin también entrenó al Maestro Roshi, pero el no tardó 3 días sino 3 años. Karin después de la muerte de Piccolo también entrena a Tenshinhan, Krilin, Yamcha y Chaoz.'),
+    Character(
+        name: 'La jeva',
+        photo:
+            'https://vignette.wikia.nocookie.net/dragonball/images/8/8a/Janemba_artwork_SDBH.png/revision/latest?cb=20200630222641&path-prefix=es',
+        race: 'Demonio',
+        age: 300,
+        origin: 'Otro mundo',
+        universe: 7,
+        occupation: 'Villano',
+        description:
+            'Janemba (ジャネンバKan, JanenbaHepJP) es un demonio de pura maldad y gran poder maligno. Creado a partir de las acciones de un adolescente ogro llamado Psyche Oni, quien era el responsable de vigilar la máquina purificadora de almas, la cual explota como consecuencia de la falta de cuidado del ogro, las almas malignas terminan por usar como huésped su cuerpo, creando así a este demonio. \nEs el antagonista principal del largometraje de Dragon Ball Z: ¡El renacer de la fusión! Goku y Vegeta.'),
+    Character(
+        name: 'Zoqueta',
+        race: 'Saiyan',
+        age: 38,
+        origin: 'Planeta tierra',
+        occupation: 'Fusión',
+        universe: 7,
+        description:
+            'En la película Dragon Ball Z: ¡El renacer de la fusión! Goku y Vegeta, la parte de las hombreras y cuello eran de color anaranjado, representado a Goku. Además, usa un pantalón blanco-grisáceos ligeramente oscurecidos que es sujetado por una cinta azul-turquesa agua y porta tobilleras también de color azul, representado el color de Vegeta. Por último, la fusión utiliza muñequeras negras de cuero y zapatos del mismo tono como calzado. En su diseño original, dibujando una línea vertical en su rostro se lo puede dividir en partes simétricas, distinguiendo a Goku en el lado derecho y a Vegeta en el izquierdo. Su cabello es similar al de Vegetto, disponiendo del peinado de ambos de sus individuos componentes en cada costado, pero teniendo un mechón en la frente en vez de dos además la terminación del cabello es en forma de pico de viuda, como el de Vegeta. Su cuerpo es similar al de Goku, es alto y delgado y al momento de transformarse en Supersaiyano mantiene el color de piel de é',
+        photo:
+            'https://i.pinimg.com/originals/e7/b6/ee/e7b6ee64b87b0b302cc0560882d80f8e.gif'),
+    Character(
+        name: 'Julieta',
+        race: 'Saiyan',
+        age: 38,
+        origin: 'Planeta tierra',
+        occupation: 'Fusión',
+        universe: 7,
+        description:
+            'En la película Dragon Ball Z: ¡El renacer de la fusión! Goku y Vegeta, la parte de las hombreras y cuello eran de color anaranjado, representado a Goku. Además, usa un pantalón blanco-grisáceos ligeramente oscurecidos que es sujetado por una cinta azul-turquesa agua y porta tobilleras también de color azul, representado el color de Vegeta. Por último, la fusión utiliza muñequeras negras de cuero y zapatos del mismo tono como calzado. En su diseño original, dibujando una línea vertical en su rostro se lo puede dividir en partes simétricas, distinguiendo a Goku en el lado derecho y a Vegeta en el izquierdo. Su cabello es similar al de Vegetto, disponiendo del peinado de ambos de sus individuos componentes en cada costado, pero teniendo un mechón en la frente en vez de dos además la terminación del cabello es en forma de pico de viuda, como el de Vegeta. Su cuerpo es similar al de Goku, es alto y delgado y al momento de transformarse en Supersaiyano mantiene el color de piel de é',
+        photo:
+            'https://i.pinimg.com/originals/e7/b6/ee/e7b6ee64b87b0b302cc0560882d80f8e.gif'),
+  ];
+  //Create Read Update Delete
+
+  Character getCharacter(String name) {
+    //TODO add code to get  a character
+  }
+
+  bool createCharacter(Character character) {
+    //TODO add code to create  a character
+  }
+  bool updateCharacter(Character character) {
+    //TODO add code to update  a character
+  }
+
+  bool deleteCharacter(String name) {
+    //TODO add code to delete  a character
+  }
+
+  List<Character> getAll({String sort, int limit, int offset}) {
+    var characters = _CHARACTERS.map((c) => c).toList();
+
+    if (sort != null) {
+      if (sort == 'name.asc') {
+        characters.sort((characterA, characterB) =>
+            characterA.name.compareTo(characterB.name));
+      }
+      if (sort == 'name.desc') {
+        characters.sort((characterA, characterB) =>
+            characterB.name.compareTo(characterA.name));
+      }
+    }
+    if (limit != null) {
+      var length = characters.length;
+
+      limit = limit > length ? length : limit;
+
+      characters = characters.sublist(offset ?? 0, (offset ?? 0) + limit);
+    }
+
+    return characters;
+  }
+}
--- lib/widgets/character_widget.dart
+++ lib/widgets/character_widget.dart
@@ -27,118 +27,111 @@ class CharacterWidget extends StatelessWidget {
                   )),
         );
       },
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.lime,
-            border: Border.all(
-              width: 5,
-              color: Colors.white,
-            )),
-        // width: screenWidth,
-        height: 200,
-        child: Stack(
-          fit: StackFit.expand,
-          //   mainAxisSize: MainAxisSize.min,
-          children: [
-            Align(
-              alignment: Alignment(0.5, 1),
-              child: Container(
-                height: 100,
-                width: screenWidth * 0.45,
-                color: Color.fromRGBO(46, 59, 91, 1),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  //   crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Container(
-                      color: Color.fromRGBO(74, 96, 248, 1),
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          character.race,
-                          style: contentStyle,
-                        ),
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment(-0.6, 1),
+            child: Container(
+              padding: EdgeInsets.only(right: 20),
+              height: 120,
+              width: screenWidth * 0.7,
+              color: Color.fromRGBO(46, 59, 91, 1),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                //   crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    color: Color.fromRGBO(74, 96, 248, 1),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        character.name,
+                        style: contentStyle,
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Flexible(
+                              child: Text(
                                 'Ocupación:',
                                 style: subTitleStyle,
                               ),
-                              Text(
+                            ),
+                            Flexible(
+                              child: Text(
                                 character.occupation,
                                 style: contentStyle,
                               ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                'Edad:',
-                                style: subTitleStyle,
-                              ),
-                              Text(
-                                '${character.age} años',
-                                style: contentStyle,
-                              ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                'Universo:',
-                                style: subTitleStyle,
-                              ),
-                              Text(
-                                'Universo-${character.universe}',
-                                style: contentStyle,
-                              ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                'Planeta:',
-                                style: subTitleStyle,
-                              ),
-                              Text(
-                                character.origin,
-                                style: contentStyle,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Edad:',
+                              style: subTitleStyle,
+                            ),
+                            Text(
+                              '${character.age} años',
+                              style: contentStyle,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Universo:',
+                              style: subTitleStyle,
+                            ),
+                            Text(
+                              'Universo-${character.universe}',
+                              style: contentStyle,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Planeta:',
+                              style: subTitleStyle,
+                            ),
+                            Text(
+                              character.origin,
+                              style: contentStyle,
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-            Align(
-              alignment: Alignment(1, 1),
-              child: SizedBox(
-                width: screenWidth / 3,
-                height: screenWidth / 3,
-                child: Image.network(
-                  character.photo,
-                  alignment: Alignment.topRight,
-                  fit: BoxFit.cover,
-                ),
+          ),
+          Align(
+            alignment: Alignment(1, 1),
+            child: SizedBox(
+              width: screenWidth / 3,
+              height: screenWidth / 3,
+              child: Image.network(
+                character.photo,
+                alignment: Alignment.topRight,
+                fit: BoxFit.cover,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
